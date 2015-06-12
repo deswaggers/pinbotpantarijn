b207f1a772f34c430cf93ab7b7e47fd5d3876dd6
deswaggers <ultiemepoep@yopmail.com>
2015-06-12 09:31:46 +0000

    Opschonen

---
+++ oefeningen/eerste_eigen_regels.py
@@ -0,0 +1,63 @@
+import procgame
+from procgame import *
+import locale
+
+# all paths
+game_path = "C:\P-ROC\pyprocgame-master\games\VXtra_start/"
+speech_path = game_path +"sound/speech/"
+sound_path = game_path +"sound/fx/"
+music_path = game_path +"sound/music/"
+dmd_path = game_path +"dmd/"
+lampshow_path = game_path +"lampshows/"
+
+class Regels1(game.Mode):
+        def __init__(self, game, priority):
+            super(Regels1, self).__init__(game, priority)
+
+        def mode_started(self):
+            print("Deze regels zijn gestart, mijn regels heten: ......(op dit moment 'Regels1, maar die gaan we hernoemen")
+            # deze variabele wordt het begin van elke bal op 0 gezet. Stel dat je een variabele wilt die
+            # per speler wordt onthouden, gebruik dan: self.game.current_player().shotcount=0. Bij de 'if' moet dit dan ook weer.
+            self.shotcount=0
+            self.check_progress()
+
+        def mode_stopped(self):
+            print("Debug, regels ..... zijn weer gestopt")
+
+## Lamps
+
+        def update_lamps(self):
+                if self.shotcount==0:
+                        self.game.effects.drive_lamp('advance_planet','slow')
+                if self.shotcount==1:
+                        self.game.effects.drive_lamp('advance_planet','medium')
+                if self.shotcount==2:
+                        self.game.effects.drive_lamp('advance_planet','fast')
+                if self.shotcount==3:
+                        self.game.effects.drive_lamp('advance_planet','on')
+
+
+## mode functions
+
+        def check_progress(self):
+                if self.shotcount==1:
+                        self.game.score(5000)
+                        # neem hier de naam van het bestand, bv. 'geluid1.wav' wordt 'geluid1'
+                        self.game.sound.play("geluid_1")
+                        self.game.effects.lower_flashers_flash()
+                elif self.shotcount==2:
+                        self.game.score(10000)
+                        self.game.sound.play("geluid2")
+                        self.game.effects.lower_flashers_flash()
+                elif self.shotcount==3:
+                        self.game.score(50000)
+                        self.game.sound.play("geluid3")
+                        self.game.effects.flashers_flash(time=2)
+                        self.shotcount=0
+                self.update_lamps()
+        
+## switches
+        def sw_advanceplanet_active(self,sw):
+                self.shotcount+=1
+                self.check_progress()
+
---
+++ oefeningen/tein.py
@@ -0,0 +1,56 @@
+import procgame
+from procgame import *
+import locale
+
+# all paths
+game_path = "C:\P-ROC\pyprocgame-master\games\VXtra_start/"
+speech_path = game_path +"sound/speech/"
+sound_path = game_path +"sound/fx/"
+music_path = game_path +"sound/music/"
+dmd_path = game_path +"dmd/"
+lampshow_path = game_path +"lampshows/"
+
+class Tein(game.Mode):
+        def __init__(self, game, priority):
+            super(Tein, self).__init__(game, priority)
+
+        def mode_started(self):
+            print("Hello, Galaxy!")
+    
+
+        def mode_stopped(self):
+            print("See ya later alligator")
+
+        def sw_Loutlane_active(self, sw):
+            self.game.effects.drive_lamp('planet1', 'fast')
+            self.game.effects.drive_lamp('planet2', 'fast')
+            self.game.effects.drive_lamp('planet3', 'fast')
+            self.game.effects.drive_lamp('planet4', 'fast')
+            self.game.effects.drive_lamp('planet5', 'fast')
+            self.game.effects.flashers_flash()
+
+        def sw_Routlane_active(self, sw):
+            print("That was very bad of you")
+            self.game.effects.drive_lamp('planet6', 'fast')
+            self.game.effects.drive_lamp('planet7', 'fast')
+            self.game.effects.drive_lamp('planet8', 'fast')
+            self.game.effects.drive_lamp('planet9', 'fast')
+            self.game.effects.drive_lamp('advance_planet', 'fast')
+            self.game.effects.flashers_flash()
+
+        def sw_Linlane_active(self, sw):
+            self.game.effects.drive_lamp('planet1', 'fast')
+            self.game.effects.drive_lamp('planet2', 'fast')
+            self.game.effects.drive_lamp('planet3', 'fast')
+            self.game.effects.drive_lamp('planet4', 'fast')
+            self.game.effects.drive_lamp('planet5', 'fast')
+
+        def sw_Rinlane_active(self, sw):
+            self.game.effects.drive_lamp('planet6', 'fast')
+            self.game.effects.drive_lamp('planet7', 'fast')
+            self.game.effects.drive_lamp('planet8', 'fast')
+            self.game.effects.drive_lamp('planet9', 'fast')
+            self.game.effects.drive_lamp('advance_planet', 'fast')
+
+##        def sw_advanceplanet_active(self, sw): 
+##            self.game.effects.flahsers_flash()
