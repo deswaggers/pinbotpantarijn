31f8dddbe29d787bdc8be3c009e861ebe86efcac
Steven <staaij@hotmail.com>
2015-12-11 10:53:31 +0100

    Mode1 ramp extra tijd toegevoegd en lampjes

--- mode_1.py
+++ mode_1.py
@@ -17,15 +17,14 @@ class Mode1(game.Mode):
         def __init__(self, game, priority):
                 super(Mode1, self).__init__(game, priority)
                 
-
         def mode_started(self):
                 self.score_layer = dmd.TextLayer(90, 20, self.game.fonts['num_09Bx7'], "center", opaque=False)
                 self.raise_layer = dmd.TextLayer(5, 2, self.game.fonts['num_09Bx7'], "left", opaque=False)
                 self.text_layer = dmd.TextLayer(5, 20, self.game.fonts['num_09Bx7'], "left", opaque=False)
-           
+                self.update_lamps()
                 ## eerst instructies in beeld, daarna na delay pas bal eruit gooien en mode beginnen
                 self.delay(name='Mode_start_na_eject', event_type=None, delay=2, handler=self.mode_start_na_eject)
-                
+
         def mode_start_na_eject(self):
                 self.game.effects.eject_ball('eject')
                 self.game.sound.play_music('music_harp', loops=-1)
@@ -57,11 +56,30 @@ class Mode1(game.Mode):
         def sw_slingL_active(self,sw):
                 self.bumpers_hit() 
                 return procgame.game.SwitchStop
+## Ramp zorgt voor extra tijd:
+        def sw_rampexit_active(self, sw):
+                self.time_left+=5
+                self.text_layer.set_text("EXTRA 5 SECONDS, " +str(self.time_left)+ "LEFT",True)
+                self.layer = dmd.GroupedLayer(128, 32, [self.text_layer])
+                self.game.sound.play("sound_hand-clap-echo")
+##Als de bal draint tijdens de mode:
+        def sw_outhole_active(self, sw):
+                self.displaytotalscore()
+                return procgame.game.SwitchStop
+        def sw_outhole_active_for_3200ms(self, sw):
+                self.game.switchedCoils.acCoilPulse('outhole_knocker',45)
+                return procgame.game.SwitchStop
 
 
 ## Lampen
-        
-
+        def update_lamps(self):
+                self.game.effects.drive_lamp('score_energy','fast')
+                self.game.effects.drive_lamp('solar_energy','medium')
+                ## 'tijdbalk' voor hoeveel tijd je nog hebt bij elke getimede mode: planeten van allemaal aan (bovenste knipperen) tot
+                ## pluto, dan voorbij als pluto uit gaat? Bij sluiten mode moet dan de 'oude staat' van het gewone spel hersteld worden.
+                
+                
+                
 ## Mode functions
         def energyflash(self):
                 self.game.coils.Solenoidselect.pulse(90)   
@@ -77,6 +95,7 @@ class Mode1(game.Mode):
                 if self.time_left<1:
                         self.displaytotalscore()
                 self.delay(name='Mode_countdown', event_type=None, delay=1, handler=self.countdown)
+                self.update_lamps()
                 
         def displaytotalscore(self):
                 self.text_layer.set_text('TOTAL SCORE '+str(self.totalscore),True)
@@ -86,6 +105,8 @@ class Mode1(game.Mode):
                 self.layer = dmd.GroupedLayer(128, 32, [self.animation_layer, self.text_layer])
                 self.delay(name='End_mode', event_type=None, delay=3, handler=self.endmode)
                 self.game.sound.fadeout_music(1500)
+                self.game.effects.drive_lamp('score_energy','off')
+                self.game.effects.drive_lamp('solar_energy','off')
         def endmode(self):
                 self.game.modes.remove(self)
                 
@@ -106,8 +127,8 @@ class Mode1(game.Mode):
                 else:
                         self.game.sound.play("sound_lasergun3")
                 ## Display
-                self.score_layer.set_text("EACH:: " +str(self.bumperscore),True)
-                self.raise_layer.set_text("RAISE AT 6: . " +str(self.bumperraise)+ 'time left:'+str(self.time_left),True) ## modetimer met healthbar/tijdbalk doen?
+                self.score_layer.set_text("EACH  " +str(self.bumperscore),True)
+                self.raise_layer.set_text("RAISE AT 6  " +str(self.bumperraise)+ 'time left:'+str(self.time_left),True) ## modetimer met healthbar/tijdbalk doen?
                 x=random.random()
                 if x>0.7:
                         self.text_layer.set_text('BAM!',True)
@@ -124,7 +145,7 @@ class Mode1(game.Mode):
                 if self.shoot_message==True:
                         self.text_layer.set_text('SHOOT THE BUMPERS',True)
                 else:
-                        self.text_layer.set_text('',True)
+                        self.text_layer.set_text('SHOOT THE RAMP',True)
                 self.score_layer.set_text("EACH:: " +str(self.bumperscore),True)
                 self.raise_layer.set_text("RAISE AT 6: . " +str(self.bumperraise)+ 'time left:'+str(self.time_left),True) ## modetimer met healthbar/tijdbalk doen?
                 anim = dmd.Animation().load(dmd_path+'DMD_Mode1_2.gif') #Als het goed is kan ie ook rechtstreeks png-bestanden aan
