b01f3285d621ad555e9a8bb59b8e95fe9b45ea87
Tein van der Lugt <teinvdlugt@gmail.com>
2016-01-12 14:15:47 +0100

    test mode 2

--- ejectmodestart.py
+++ ejectmodestart.py
@@ -70,7 +70,7 @@ class EjectModestart(game.Mode):
 
         self.next_mode = random.choice(unplayed_modes)
 
-        self.next_mode = 2  # Mode 3 for testing
+        self.next_mode = 1  # Mode 2 for testing
 
     def sw_rampexit_active(self, sw):
         if self.game.current_player().mode_running == False and self.mode_enabled == False:
