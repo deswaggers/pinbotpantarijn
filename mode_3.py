249d1574906b379a1f5e8b225c28e5bc06bff091
Tein van der Lugt <teinvdlugt@gmail.com>
2016-01-12 13:58:09 +0100

    mode_3 cleanup

--- mode_3.py
+++ mode_3.py
@@ -27,7 +27,8 @@ class Mode3(game.Mode):
         self.raise_layer = dmd.TextLayer(5, 2, self.game.fonts['num_09Bx7'], "left", opaque=False)
         self.text_layer = dmd.TextLayer(5, 20, self.game.fonts['num_09Bx7'], "left", opaque=False)
         self.update_lamps()
-        ## eerst instructies in beeld, daarna na delay pas bal eruit gooien en mode beginnen
+
+        # Eerst instructies in beeld, daarna na delay pas bal eruit gooien en mode beginnen
         self.visor_count = 1
         self.visor_check()
         self.hit()
@@ -36,13 +37,6 @@ class Mode3(game.Mode):
         self.game.sound.play_music('music_starwars_intro', loops=-1)
         self.game.current_player().mode_running = False
         self.layer = None
-
-    ## niet nodig voor ons plan/project.
-    ##        def drops_reset(self):
-    ##                self.game.coils.Drops_RightInsBFlash.pulse(120)
-    ##                self.dropscount=0
-    ##                self.drop_timer = 0
-    ##                self.game.effects.drive_lamp('drops','medium')
 
     def verticaal_aan1(self):
         for i in self.yellow:
@@ -121,74 +115,3 @@ class Mode3(game.Mode):
             self.visor_check()
         elif self.visor_count == 5 and self.game.switches.visor5.is_active():
             self.verticaal_uit5()
-
-
-
-
-            ##        def drops_check(self):
-            ##                if self.dropscount==0:
-            ##                        self.dropscount=1
-            ##                        self.game.score(200)
-            ##                        #self.cancel_delayed('drop_timer')
-            ##                        print "eerste droptarget"
-            ##                        self.delay(name='drop_timer', event_type=None, delay=6, handler=self.drops_reset)
-            ##
-            ##
-            ##                elif self.dropscount==1:
-            ##                        self.dropscount=2
-            ##                        self.game.score(1000)
-            ##                        #self.cancel_delayed('drop_timer')
-            ##                        self.delay(name='drop_timer', event_type=None, delay=6, handler=self.drops_reset)
-            ##                        print "2e droptarget"
-            ##
-            ##                elif self.dropscount==2:
-            ##                        self.game.score(10000)
-            ##                        print "3e droptarget"
-            ##                        self.cancel_delayed('drop_timer')
-            ##                        self.drops_reset()
-            ##                self.game.sound.play("sound_lasergun1")
-            ##                self.update_lamps()
-
-    def update_lamps(self):
-        if self.game.switches.droptarget1.is_active() or self.game.switches.droptarget2.is_active() or self.game.switches.droptarget3.is_active():
-            if self.game.switches.droptarget1.is_active():
-                self.game.effects.drive_lamp('droptop', 'on')
-            elif self.drop_timer >= 4:
-                self.game.effects.drive_lamp('droptop', 'fast')
-            elif self.drop_timer >= 2:
-                self.game.effects.drive_lamp('droptop', 'medium')
-            else:
-                self.game.effects.drive_lamp('droptop', 'slow')
-
-            if self.game.switches.droptarget2.is_active():
-                self.game.effects.drive_lamp('dropmid', 'on')
-            elif self.drop_timer >= 4:
-                self.game.effects.drive_lamp('dropmid', 'fast')
-            elif self.drop_timer >= 2:
-                self.game.effects.drive_lamp('dropmid', 'medium')
-            else:
-                self.game.effects.drive_lamp('dropmid', 'slow')
-
-            if self.game.switches.droptarget3.is_active():
-                self.game.effects.drive_lamp('dropbottom', 'on')
-            elif self.drop_timer >= 4:
-                self.game.effects.drive_lamp('dropbottom', 'fast')
-            elif self.drop_timer >= 2:
-                self.game.effects.drive_lamp('dropbottom', 'medium')
-            else:
-                self.game.effects.drive_lamp('dropbottom', 'slow')
-        else:
-            self.game.effects.drive_lamp('drops', 'slow')
-
-
-
-            ## switches
-
-    def sw_droptarget1_active_for_200ms(self, sw):
-        self.drops_check()
-
-    def sw_droptarget2_active_for_200ms(self, sw):
-        self.drops_check()
-
-    def sw_droptarget3_active_for_200ms(self, sw):
-        self.drops_check()
