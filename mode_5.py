7181866b341c30a2fab0d5473e1b60590c9d26fb
USB1 <sstaaij@pantarijn.nl>
2016-10-27 11:53:50 +0200

    mode5

--- mode_5.py
+++ mode_5.py
@@ -13,20 +13,20 @@ dmd_path = game_path + "dmd/"
 
 class Mode5(game.Mode):
     def __init__(self, game, priority):
-        super(Mode5, self).__init__(game, priority)
+       super(Mode5, self).__init__(game, priority)
 
    def mode_started(self):
-        self.instruction_layer = dmd.TextLayer(30, 20, self.game.fonts['num_07x4'], opaque=False)
-        self.display_instructions()
-        self.delay(name='start_mode2', event_type=None, delay=2, handler=self.startmode2)
+       self.instruction_layer = dmd.TextLayer(30, 20, self.game.fonts['num_07x4'], opaque=False)
+       self.display_instructions()
+       self.delay(name='start_mode2', event_type=None, delay=2, handler=self.startmode2)
 
 
    def startmode2(self):
-        self.game.effects.eject_ball('eject')
-        self.game.sound.play_music('music_starwars_cantina_band', loops=-1)
+       self.game.effects.eject_ball('eject')
+       self.game.sound.play_music('music_starwars_cantina_band', loops=-1)
 
 
    def mode_stopped(self):
-        self.layer = None
-        if self.game.switches.outhole.is_active():
-            self.game.switchedCoils.acCoilPulse('outhole_knocker',45)
+       self.layer = None
+       if self.game.switches.outhole.is_active():
+           self.game.switchedCoils.acCoilPulse('outhole_knocker',45)
