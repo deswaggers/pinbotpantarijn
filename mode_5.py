830c14c76082e34117d3f1fb7210c52da42c696f
USB1 <sstaaij@pantarijn.nl>
2016-11-24 11:37:13 +0100

    test met mode 5 - softwareteam

--- mode_5.py
+++ mode_5.py
@@ -20,11 +20,12 @@ class Mode5(game.Mode):
         self.instruction_layer = dmd.TextLayer(30, 20, self.game.fonts['num_07x4'], opaque=False)
         self.display_instructions()
         self.delay(name='start_mode2', event_type=None, delay=2, handler=self.startmode5)
+        self.numberHits = 0
 
 
     def startmode5(self):
         self.game.effects.eject_ball('eject')
-        self.game.sound.play_music('music_starwars_cantina_band', loops=-1)
+        self.game.sound.play_music('music_harp', loops=-1)
 
 
     def mode_stopped(self):
@@ -36,6 +37,21 @@ class Mode5(game.Mode):
     def display_instructions(self):
         self.instruction_layer.set_text('First instructions here')
         self.layer = self.instruction_layer
+
+    
+    def reset_multiplier(self):
+        if self.numberHits>0:
+            self.numberHits-=1
+            self.game.sound.play("sound_outlane")
+            
+
+    def sw_Rbank1_active(self, sw):
+        self.numberHits += 1
+        self.game.score(1000 * (2**self.numberHits))
+        self.delay(name='reset_multiplier', event_type=None, delay=5, handler=self.reset_multiplier)
+        self.game.sound.play("sound_hit")
+
+    
 
     def sw_outhole_active(self, sw):
         self.game.current_player().stop_eject_mode_mode(self)
