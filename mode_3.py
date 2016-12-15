8d9075ef3ae7a25d0f28787eb823ea78e7c5ca7f
Viktor Loomans <612121@live.pantarijn.nl>
2016-12-15 11:44:33 +0100

    update

--- mode_3.py
+++ mode_3.py
@@ -18,14 +18,14 @@ class Mode3(game.Mode):
     def mode_started(self):
         self.instruction_layer = dmd.TextLayer(30, 20, self.game.fonts['num_07x4'], opaque=False)
         self.display_instructions()
-        self.delay(name='start_mode2', event_type=None, delay=2, handler=self.startmode2)
-
+        self.delay(name='start_mode3', event_type=None, delay=2, handler=self.startmode3)
+        self.balingat=0
+        self.schepenkapot = 0
+        
     def startmode3(self):
         self.game.effects.eject_ball('eject')
         self.game.sound.play_music('music_starwars_cantina_band', loops=-1)
-        balingat=0
-        health=75
-
+        
     def mode_stopped(self):
         self.layer = None
         if self.game.switches.outhole.is_active():
@@ -36,18 +36,35 @@ class Mode3(game.Mode):
         self.game.sound.play("sound_hand-clap-echo")
         self.game.effects.drive_lamp('score_energy','slow')
 
+    def sw_eject_active(self, sw):
+        return procgame.game.SwitchStop
 
-'''
-    def sw_flipperLwL_active
-        if balingat = 1:
-            health-=1
+    def sw_eject_active_for_500ms(self, sw):
+        self.balingat=1
+        self.health=30
+        self.delay(name='tijd', event_type=None, delay=5, handler=self.endTime)
+        return procgame.game.SwitchStop
+
+    def endTime(self):
+        if self.health <=0:
+            self.game.sound.play("sound_outlane")
+            #moet nog veranderd worden
+            self.schepenkapot += 1
+            self.game.score(420000*self.schepenkapot)
+        else:
+            self.game.sound.play("sound_outlane")
+            #moet nog veranderd worden
+        self.game.effects.eject_ball('eject')
+
+    def sw_flipperLwL_active(self,sw)
+        if self.balingat = 1:
+            self.health-=1
 
     def sw_flipperLwR
-        if balingat = 1:
-            health-=1
+        if self.balingat = 1:
+            self.health-=1
 
-    def 
-'''
+
 
 
         
