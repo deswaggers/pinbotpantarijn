4bf41a5c42a82ea1d201a5f7d6e14ccd02b73c94
Wouter Kortleve <612216@live.pantarijn.nl>
2016-12-20 15:24:25 +0100

    mode 3 4.2 en mode 2 plaatje

--- mode_4.py
+++ mode_4.py
@@ -25,7 +25,8 @@ class Mode4(game.Mode):
         self.delay(name='start_mode2', event_type=None, delay=2, handler=self.startmode2)
         self.rampCount=0
         self.game.sound.play("sound_2017_houston_we_got")
-        img = Image.open("images_for_beamer/shoot_ramp.jpg")
+        img = Image.open("images_for_beamer/shoot_ramp.png")
+        img.format = "PNG") 
         img.show()
         
 
