039d137c94731306b7745eda013a74484d6afcfc
Wouter Kortleve <612216@live.pantarijn.nl>
2016-12-20 09:44:13 +0100

    mode3 gefixt

--- mode_3.py
+++ mode_3.py
@@ -16,8 +16,6 @@ class Mode3(game.Mode):
         super(Mode3, self).__init__(game, priority)
 
     def mode_started(self):
-        self.instruction_layer = dmd.TextLayer(30, 20, self.game.fonts['num_07x4'], opaque=False)
-        self.display_instructions()
         self.delay(name='start_mode3', event_type=None, delay=2, handler=self.startmode3)
         self.balingat=0
         self.schepenkapot = 0
@@ -27,7 +25,6 @@ class Mode3(game.Mode):
         self.game.sound.play_music('music_starwars_cantina_band', loops=-1)
         
     def mode_stopped(self):
-        self.layer = None
         if self.game.switches.outhole.is_active():
             self.game.switchedCoils.acCoilPulse('outhole_knocker',45)
 
@@ -47,7 +44,7 @@ class Mode3(game.Mode):
 
     def endTime(self):
         if self.health <=0:
-            self.game.sound.play("sound_outlane")
+            self.game.sound.play("sound_2017_biem")
             #moet nog veranderd worden
             self.schepenkapot += 1
             self.game.score(420000*self.schepenkapot)
