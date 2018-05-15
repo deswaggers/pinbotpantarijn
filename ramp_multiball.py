4d1fa48a441eccdd469fef77aa03eb15a5843457
USB1 <sstaaij@pantarijn.nl>
2018-05-15 11:24:02 +0200

    ramp multiball verder gewerkt

--- ramp_multiball.py
+++ ramp_multiball.py
@@ -19,7 +19,7 @@ class RampMultiball(game.Mode):
 
     def mode_started(self):
         print "RampMultiball uit ramp_multiball.py is gestart"
-        self.instruction_layer = dmd.TextLayer(30, 20, self.game.fonts['num_07x4'], opaque=False)
+        self.instruction_layer = dmd.TextLayer(30, 20, self.game.fonts['num_14x10'], opaque=False)
         #self.game.lampctrl.register_show('multiball_start', lampshow_path +"planeten_short.lampshow")
         #self.game.lampctrl.register_show('visor_lampshow', lampshow_path +"Pinbot_1.lampshow")
         self.delay(name='start_rampMB', event_type=None, delay=5, handler=self.start_rampMB)
@@ -40,6 +40,7 @@ class RampMultiball(game.Mode):
     def stop_rampmultiball(self):
         self.game.sound.play_music('music_2017_creepy_alien_music')
         self.game.modes.remove(self)
+        self.layer = None
 
 
     def mode_stopped(self):
@@ -50,6 +51,12 @@ class RampMultiball(game.Mode):
     def display_instructions(self):
         self.instruction_layer.set_text('RAMP MULTIBALL GESTART')
         self.layer=dmd.GroupedLayer(128,32,[self.instruction_layer])
+
+
+    def sw_shooterLane_open_for_500ms(self,sw):
+        self.game.coils.RvisorGI.schedule(schedule=0x0f0f0f0f, cycle_seconds=2, now=True)
+        self.game.coils.LvisorGI.schedule(schedule=0xf0f0f0f0, cycle_seconds=2, now=True)
+        self.game.effects.ramp_down()
 
 
     def sw_outhole_active(self, sw):
