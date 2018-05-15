ddf5f0fa4804e108363c490649d213411174d516
USB1 <sstaaij@pantarijn.nl>
2018-05-15 11:33:28 +0200

    ramp multiball verder gewerkt

--- ramp_multiball.py
+++ ramp_multiball.py
@@ -24,6 +24,7 @@ class RampMultiball(game.Mode):
         #self.game.lampctrl.register_show('visor_lampshow', lampshow_path +"Pinbot_1.lampshow")
         self.delay(name='start_rampMB', event_type=None, delay=5, handler=self.start_rampMB)
         self.display_instructions()
+        self.twoballsinplay = False
 
 
     def start_rampMB(self):
@@ -31,11 +32,11 @@ class RampMultiball(game.Mode):
         self.game.trough.launch_balls(1)
         self.game.sound.play_music('music_harp', loops=-1)
 
-    '''
+
     def mode_tick(self):
-        if (self.game.trough.num_balls_in_play<2):
+        if ((self.game.trough.num_balls_in_play<2) && self.twoballsinplay):
             self.stop_rampmultiball()
-    '''
+
 
     def stop_rampmultiball(self):
         self.game.sound.play_music('music_2017_creepy_alien_music')
@@ -57,6 +58,7 @@ class RampMultiball(game.Mode):
         self.game.coils.RvisorGI.schedule(schedule=0x0f0f0f0f, cycle_seconds=2, now=True)
         self.game.coils.LvisorGI.schedule(schedule=0xf0f0f0f0, cycle_seconds=2, now=True)
         self.game.effects.ramp_up()
+        self.twoballsinplay = True
 
 
     def sw_outhole_active(self, sw):
